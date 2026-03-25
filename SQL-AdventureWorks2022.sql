a19c4ddb1a91cc5042819727fe72230391a889e5
Viacheslav <east.slava@gmail.com>
2026-03-25 13:51:46 +1300

    update sql AdventureWorks2022

--- SQL-AdventureWorks2022.sql
+++ SQL-AdventureWorks2022.sql
@@ -16,3 +16,11 @@ SELECT
 FROM Production.Product
 WHERE SellStartDate IS NOT NULL 
     AND ProductLine = 'T';
+
+SELECT 
+    st.Name AS Territory, 
+    SUM(od.LineTotal) AS TotalRevenue
+FROM Sales.SalesOrderHeader AS oh
+JOIN Sales.SalesOrderDetail AS od ON oh.SalesOrderID = od.SalesOrderID
+JOIN Sales.SalesTerritory AS st ON oh.TerritoryID = st.TerritoryID
+GROUP BY st.Name;
