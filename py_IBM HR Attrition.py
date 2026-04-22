7cb03a81cc013f8ce20f5f27dae89432feaa3cff
Viacheslav <east.slava@gmail.com>
2026-04-23 10:35:40 +1200

    update IBM HR Attrition

--- py_IBM HR Attrition.py
+++ py_IBM HR Attrition.py
@@ -13,3 +13,15 @@ df = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
 
 # Preview data
 print(df.head())
+
+# ==============================
+# 3. Basic Info
+# ==============================
+print(df.info())
+print(df.describe())
+
+# ==============================
+# 4. Check Attrition Distribution
+# ==============================
+attrition_counts = df['Attrition'].value_counts()
+print(attrition_counts)
