611e8c5d5aac546b622b0f70c0905632f7ad4e62
Viacheslav <east.slava@gmail.com>
2026-06-24 17:54:53 +1200

    refresh files

--- excel_remove_dubles_2.vb
+++
@@ -1,5 +0,0 @@
-Set objExcel=Createobject("Excel.Application")
-Set objWbk=objExcel.Workbooks.Open("FilePath")
-Set objSht = objWbk.WorkSheets(1)
-objExcel.Visible=True
-objSht.UsedRange.RemoveDuplicates 1,1
